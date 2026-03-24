748728d058cd8306488f5ebc4d8eeeec72822530
Deepak Devaraj <deepaksam@gmail.com>
2026-03-24 14:29:08 +0000

    update app.py for flake8

--- app/app.py
+++ app/app.py
@@ -2,14 +2,20 @@ from flask import Flask
 from prometheus_flask_exporter import PrometheusMetrics
 import os
 
+
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
+
 
 @app.route("/")
 def home():
     env_name = os.getenv("ENV_NAME", "local")
     api_key = os.getenv("API_KEY", "not-set")
-    return f"Hello from Kubernetes! ENV={env_name}, API_KEY_PRESENT={api_key != 'not-set'}"
+    return (
+            f"Hello from Kubernetes! "
+            f"ENV={env_name}, API_KEY_PRESENT={api_key != 'not-set'}"
+    )
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
